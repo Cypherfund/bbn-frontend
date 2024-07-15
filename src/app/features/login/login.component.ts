c5f3dc5129a632dae81cda0660a8b8e38132a8bf
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-07-15 23:00:15 +0100

    add login and signup

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../services/user/user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  formGroup!: FormGroup;
+  inProgress: boolean = false
+  subscriptions: Subscription[] = [];
 
+  constructor(private fb: FormBuilder,
+              private userService: UserService) {
+    this.formGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.subscriptions.push(this.userService.login$.subscribe(loginVal => {
+      if(loginVal === 1)
+        this.userService.navigateToHome();
+    }));
+  }
+
+  submitForm() {
+    if (this.formGroup.valid) {
+
+      this.inProgress = true;
+
+      const subscription = this.userService.loginUser({
+        usernameOrEmailOrPhone: this.formGroup.value.email,
+        password: this.formGroup.value.password
+      }).subscribe();
+
+      subscription.add(() => (this.inProgress = false));
+
+      this.subscriptions.push(subscription);
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
 }
--- src/app/features/signup/signup.module.ts
+++ src/app/features/signup/signup.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {UserService} from "../../services/user/user.service";
+import {Router} from "@angular/router";
+import {Signup} from "../../models/signup";
 
 
 @NgModule({
@@ -14,4 +19,74 @@ import { SignupComponent } from './signup.component';
     SignupRoutingModule
   ]
 })
-export class SignupModule { }
+export class SignupModule {
+  signupForm!: FormGroup;
+  verificationCode!: string;
+  private spinner!: boolean;
+  stage = 'signup';
+  private subscriptions: Subscription[] = []
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private userService: UserService) {
+    this.signupForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      lastName: [''],
+      firstName: ['']
+    });
+    this.subscriptions.push(this.userService.login$.subscribe(loginVal => {
+      if(loginVal === 1)
+        this.userService.navigateToHome();
+    }));
+  }
+
+  submitForm() {
+    if (this.signupForm.valid) {
+      this.spinner = true
+      const user: Signup = {
+        name: this.signupForm.value.firstName + ' ' + this.signupForm.value.lastName,
+        username: this.signupForm.value.firstName,
+        email: this.signupForm.value.email,
+        phone: '',
+        password: this.signupForm.value.password,
+        roles: ['CUSTOMER']
+      };
+      const subscription = this.userService.registerUser(user).subscribe();
+
+      subscription.add(() => (this.spinner = false));
+
+      this.subscriptions.push(subscription);
+    }
+
+  }
+
+  onSubmitCodeVerification() {
+    if (this.verificationCode) {
+      // Call a service to verify the code
+      this.verifyCode(this.verificationCode);
+    }
+  }
+
+  verifyCode(code: string) {
+    // Implement the logic to verify the code
+    // For example, call an API endpoint to verify the code
+    console.log('Verifying code:', code);
+    // Simulate verification success
+    alert('Code verified successfully!');
+  }
+
+  resendCode() {
+    // Implement the logic to resend the verification code
+    // For example, call an API endpoint to resend the code
+    console.log('Resending code');
+    // Simulate resending success
+    alert('Verification code resent!');
+  }
+
+  ngOnDestroy(): void {
+    for (const sub of this.subscriptions) {
+      sub.unsubscribe();
+    }
+  }
+}
