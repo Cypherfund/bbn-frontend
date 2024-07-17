5d87b8abd0ac38227adb0731c3c4b7064f9f3c97
E.Ngai <elizabeth.ngai@dme.systems>
2024-07-17 20:01:19 +0100

    update css

--- src/app/features/bbn/bbn-routing.module.ts
+++ src/app/features/bbn/bbn-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BbnComponent } from './bbn.component';
 
-const routes: Routes = [{ path: '', component: BbnComponent }];
+const routes: Routes = [
+  { path: '', component: BbnComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
