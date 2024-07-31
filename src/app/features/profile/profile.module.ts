5009258bb331d365b283ef6148f96909b869a2f6
E.Ngai <elizabeth.ngai@dme.systems>
2024-07-31 12:20:01 +0100

    update profile view

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -6,6 +6,10 @@ import {MatButtonModule} from "@angular/material/button";
 import {RouterModule, Routes} from "@angular/router";
 import {MatTableModule} from "@angular/material/table";
 import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatLineModule} from "@angular/material/core";
 
 const routes: Routes = [
   {path: '', component: UserProfileComponent}
@@ -19,8 +23,11 @@ const routes: Routes = [
     CommonModule,
     MatCardModule,
     MatButtonModule,
-    MatTableModule,
     MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+    MatLineModule,
     RouterModule.forChild(routes)
   ]
 })
