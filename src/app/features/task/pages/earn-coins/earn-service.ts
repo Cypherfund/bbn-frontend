d8bf87abbd94b23273ed061386b318a680c42c98
E.Ngai <elizabeth.ngai@dme.systems>
2024-08-05 11:40:31 +0100

    fix

--- src/app/features/task/pages/earn-coins/earn-service.ts
+++ src/app/features/task/pages/earn-coins/earn-service.ts
@@ -1,11 +1,13 @@
-import {Injectable} from "@angular/core";
+import {Inject, Injectable, PLATFORM_ID} from "@angular/core";
+import {isPlatformBrowser} from "@angular/common";
 
 
 @Injectable()
 export class EarnService {
   divWidth = 50;
   intervalId: any;
-  constructor() {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId))
     this.startResizing();
   }
 
