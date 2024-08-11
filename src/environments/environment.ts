3bb3b0c88e2864a4317259d94dd1c9c1c5812a2d
ekuty2001@gmail.com <ekuty2001@gmail.com>
2024-08-11 08:15:14 +0100

    changed IP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   envName: 'production',
-  apiUrl: 'https://games.cypherfund.me',
+  apiUrl: 'https://159.89.242.197',
   imagebucket: 'assets/images/'
 };
