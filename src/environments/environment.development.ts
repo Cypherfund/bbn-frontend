e596c682fa48c96acadbb6ac2e52682747342ed5
ekuty2001@gmail.com <ekuty2001@gmail.com>
2024-08-11 12:24:40 +0100

    add images to splash folder inside assets and update enviromental variable

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   envName: 'production',
-  apiUrl: 'https://games.cypherfund.me',
+  apiUrl: 'http://159.89.242.197',
   imagebucket: 'assets/images/',
   paymentApiUrl: 'http://159.89.242.197'
 };
