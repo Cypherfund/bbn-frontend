d42033162db1ee615dcba62d3a8ae794e9c3a1c5
ekuty2001@gmail.com <ekuty2001@gmail.com>
2024-08-11 20:38:15 +0100

    Add tips logic

--- src/app/features/micro-task/tips-screen/tips-screen.component.ts
+++ src/app/features/micro-task/tips-screen/tips-screen.component.ts
@@ -7,6 +7,27 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tips-screen.component.scss'
 })
 export class TipsScreenComponent {
-  @Input() tips : Tips[] = []
+  @Input() tips : Tips[] = [];
+
+  activeTipsIndex = 0
+  tipsToShow = this.tips[this.activeTipsIndex]
+ 
+  nav = false
+
+  moveToNextTips = () => {
+    const numOfTips = this.tips.length
+    if (this.activeTipsIndex < numOfTips) {
+      this.activeTipsIndex += 1
+
+      if(this.activeTipsIndex === numOfTips) {
+        this.activeTipsIndex -= 1
+        this.nav = true
+      }
+    }
+   if (this.nav) {
+    console.log('moving to sign up page');
+   }
+
+  }
   
 }
