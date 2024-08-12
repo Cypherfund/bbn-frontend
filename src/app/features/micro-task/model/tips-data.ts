707b41dc4cc6d76a9f34f686c2df98b1b5d059ae
ekuty2001@gmail.com <ekuty2001@gmail.com>
2024-08-12 17:01:01 +0100

    add styles for micro-task component, tips-component and edited model and assets

--- src/app/features/micro-task/model/tips-data.ts
+++ src/app/features/micro-task/model/tips-data.ts
@@ -14,12 +14,12 @@ const tips : Tips[] = [
   {
     img : 'assets/micro-task/spash/t2.png',
   heading : 'Refer a friend and earn',
-  tip : 'complete your profile to enjoy, unlimited access to our premium packages'
+  tip : 'Gain more by referring to friends and family'
   },
   {
-    img : 'assets/micro-task/spash/t1.png',
+    img : 'assets/micro-task/spash/t3.png',
     heading : 'Subscription',
-    tip : 'complete your profile to enjoy, unlimited access to our premium packages'
+    tip : 'Elevate your package to gain access to unlimited features'
   },
 
   
